db500403529020cdd0983be4fe28c6d950f7046e
Charles Grunwald <cgrunwald@gmail.com>
2026-09-14 00:41:57 +0100

    Add one more MSVC fix

--- src/lj_gc2token.h
+++ src/lj_gc2token.h
@@ -971,7 +971,7 @@ typedef struct LJGC2RootRange {
   void *hi;
 } LJGC2RootRange;
 
-typedef struct LJGC2RootDesc {
+typedef struct  LJ_ALIGN(16) LJGC2RootDesc {
   uint64_t control;  /* generation << 2 | LJGC2RootDescState. */
   uint32_t flags;
   uint32_t reserved;
@@ -988,7 +988,7 @@ typedef struct LJGC2RootDesc {
   ** coverage after owner-written payload to reduce control-word false sharing.
   */
   la_u128 coverage;
-} __attribute__((aligned(16))) LJGC2RootDesc;
+} LJGC2RootDesc;
 
 typedef struct LJGC2RootDescSpec {
   uint32_t flags;
